abff2a6a2d7a3c225d06905fb11145871c8c6c9d
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2023-01-19 00:45:51 -0600

    uploaded semi done website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./componets/Navbar";
+import Home from "./componets/Home";
+import "./App.css"
+import { Redirect, Route, Router, Switch } from "react-router-dom";
+import Footer from "./componets/Footer";
+import About from "./componets/About";
+import Resume from "./componets/Resume";
+import Contact from "./componets/Contact";
+import Portfolio from "./componets/Portfolio";
+import Blog from "./componets/Blog";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+        <Route path="/blog">
+          <Blog/>
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
